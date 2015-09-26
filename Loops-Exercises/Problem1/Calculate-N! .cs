61da0b017713a6aea41e48524c37430a19b94f8c
Daniel Kordev <danieleto.play@gmail.com>
2015-09-26 16:38:24 +0300

    SoftUni Projects

---
+++ Loops-Exercises/Problem1/Calculate-N! .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Calculate_N 
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial = factorial * i;                
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
